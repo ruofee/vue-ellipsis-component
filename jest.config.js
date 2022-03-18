2c254af387838acd66fefced83059101667651ea
ruofee <ruofee@126.com>
2022-03-18 22:35:58 +0800

    chore: vitest coverage

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
-  collectCoverage: true,
-  collectCoverageFrom: [
-    '**/packages/**/*.{ts,vue}',
-  ],
-}
