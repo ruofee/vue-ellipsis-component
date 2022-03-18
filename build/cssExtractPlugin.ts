47f6fa3cabe014feafd380489067771c27f5131c
ruofee <ruofee@126.com>
2022-03-18 22:28:26 +0800

    chore: using vitest as unit test tool, and supporting lint  in development

--- build/cssExtractPlugin.ts
+++ build/cssExtractPlugin.ts
@@ -1,66 +1,32 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import fs from 'fs'
-import { resolve } from 'path'
-import type { ResolvedConfig, PluginOption } from 'vite'
+import type { PluginOption } from 'vite';
 
-const fileRegex = /\.(css)$/
-const injectCode = (code: string) =>
-  `function styleInject(css,ref){if(ref===void 0){ref={}}var insertAt=ref.insertAt;if(!css||typeof document==="undefined"){return}var head=document.head||document.getElementsByTagName("head")[0];var style=document.createElement("style");style.type="text/css";if(insertAt==="top"){if(head.firstChild){head.insertBefore(style,head.firstChild)}else{head.appendChild(style)}}else{head.appendChild(style)}if(style.styleSheet){style.styleSheet.cssText=css}else{style.appendChild(document.createTextNode(css))}};styleInject(\`${code}\`)`
-const template = `console.warn("__INJECT__")`
+let packageNames: Array<string> = [];
+let viteConfig;
+let IIFEcss;
 
-let viteConfig: ResolvedConfig
-const css: string[] = []
-
-export default function libInjectCss(): PluginOption {
+// 将 css 打包到 js 文件中
+export default function(): PluginOption {
   return {
-    name: 'lib-inject-css',
-
     apply: 'build',
-
-    configResolved(resolvedConfig: ResolvedConfig) {
-      viteConfig = resolvedConfig
+    enforce: 'post',
+    name: 'ViteSingleCssPlugin',
+    configResolved(config) {
+      viteConfig = config;
+      packageNames = viteConfig.build.lib.formats.map(format => viteConfig.build.lib.fileName(format));
     },
-
-    transform(code: string, id: string) {
-      if (fileRegex.test(id)) {
-        css.push(code)
-        return {
-          code: '',
-        }
+    generateBundle(_, bundle) {
+      const cssFileName = 'style.css';
+      const { [cssFileName]: cssBundle } = bundle;
+      if (cssBundle) {
+        IIFEcss = `(function() {try {var elementStyle = document.createElement('style');elementStyle.innerText = ${JSON.stringify((cssBundle as any).source)};document.head.appendChild(elementStyle);} catch(error) {console.error(error, 'unable to concat style inside the bundled file')}})()`;
+        delete bundle[cssFileName];
       }
-      if (
-        // @ts-ignore
-        id.includes(viteConfig.build.lib.entry)
-      ) {
-        return {
-          code: `${code}
-          ${template}`,
+      packageNames.forEach(packageName => {
+        if (bundle[packageName]) {
+          const taregtBundle: any = bundle[packageName];
+          taregtBundle.code += IIFEcss;
         }
-      }
-      return null
-    },
-
-    async writeBundle(_: any, bundle: any) {
-      for (const file of Object.entries(bundle)) {
-        const { root } = viteConfig
-        const outDir: string = viteConfig.build.outDir || 'dist'
-        const fileName: string = file[0]
-        const filePath: string = resolve(root, outDir, fileName)
-
-        try {
-          let data: string = fs.readFileSync(filePath, {
-            encoding: 'utf8',
-          })
-
-          if (data.includes(template)) {
-            data = data.replace(template, injectCode(css.join('\n')))
-          }
-
-          fs.writeFileSync(filePath, data)
-        } catch (e) {
-          console.error(e)
-        }
-      }
-    },
+      })
+    }
   }
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,21 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config'
 import { createVuePlugin } from 'vite-plugin-vue2';
+import VitePluginEslint from 'vite-plugin-eslint';
 import cssExtractPlugin from './build/cssExtractPlugin';
 import packageJson from './package.json';
 const path = require('path');
 
-const isProd = process.env.APP_ENV === 'production'
-
 export default defineConfig({
+  test: {
+    environment: 'jsdom',
+    global: true
+  },
   build: {
     outDir: path.resolve(__dirname, './lib'),
     lib: {
       entry: path.resolve(__dirname, './packages/main.ts'),
       name: packageJson.name,
+      formats: ['umd', 'es'],
       fileName: (format) => {
         if (format === 'umd') {
           return `${packageJson.name}.js`;
@@ -29,5 +33,11 @@ export default defineConfig({
       }
     }
   },
-  plugins: [createVuePlugin(), isProd && cssExtractPlugin()]
+  plugins: [
+    VitePluginEslint({
+      include: ['packages/**/*.ts', 'packages/**/*.vue']
+    }),
+    createVuePlugin(),
+    cssExtractPlugin()
+  ]
 });
